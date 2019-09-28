8ad48427b992a825b54f2f172b100d8ac1426013
Peter(ChaoHui) Chen <chenpeter0821@gmail.com>
2019-09-28 18:22:11 -0400

    add the game key button

--- Python/alien_invasion/setting.py
+++ Python/alien_invasion/setting.py
@@ -8,4 +8,7 @@ class Settings():
         #Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.big_color = (230,230,230)
+        self.big_color = (255,255,255)
+
+        #ship setting
+        self.ship_speed_factor = 1.5
